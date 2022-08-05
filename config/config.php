c2300c6337e42aa9aaca9129231d782bf0129a1d
ealpha072 <ealpha072@gmail.com>
2022-08-05 21:27:31 +0300

    Connection to remote and local server

--- config/config.php
+++ config/config.php
@@ -1,33 +1,38 @@
 <?php 
-session_start();
+//session_start();
+// echo ($_SERVER['HTTP_HOST']);
+// echo ($_SERVER['SERVER_NAME']);
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+var_dump($cleardb_url);
 
 class Database{
-    private $host = "";
-    private $dbname = "";
-    private $username = "";
-    private $password = "";
-    public $conn;
+    //Heroku creds
+    public $host       = "us-cdbr-east-06.cleardb.net";
+    public $username   = "b9e5bb4b4835e3";
+    public $password   = "5719819c";
+    public $dbname     = "heroku_bece3e49d0c6d9b";
+    //local creds
+    // public $host       = "us-cdbr-east-06.cleardb.net";
+    // public $username   = "b9e5bb4b4835e3";
+    // public $password   = "5719819c";
+    // public $dbname     = "heroku_bece3e49d0c6d9b";
+    private $conn; 
 
-    public function getConn(){
-        if($_SERVER['HTTP_HOST']=== 'localhost'){
-            $host = "";
-            $dbname = "";
-            $username = "";
-            $password =  "";
-            try {
-                //code...
-                $this->conn = new PDO("mysql:host={$this->host};{$this->dbname};{$this->username};{$this->password}");
-            } catch (Exception $e) {
-                //throw $th;
-                throw new Exception (($e->getMessage()));
-            }
-
-            return $this->conn;
+    public function getCon(){
+        try {
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->dbname}", $this->username, $this->password);
+            // set the PDO error mode to exception
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+        } catch(PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
         }
+        return $this->conn;
     }
-
 }
 
+$db = new Database();
+$db->getCon();
 
 
 ?>
