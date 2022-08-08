b6d8863cf245f4499a2e1de64bf31cc1431e140b
ealpha072 <ealpha072@gmail.com>
2022-08-08 23:18:08 +0300

    Create tables messages, notices, drugs,expenses and expense categor

--- tables.sql
+++ tables.sql
@@ -79,3 +79,71 @@ CREATE TABLE `hospital_db`.`services` (
     `date_created` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP , 
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
+
+CREATE TABLE `hospital_db`.`suppliers` (
+    `id` INT(255) NOT NULL AUTO_INCREMENT , 
+    `supplier_id` VARCHAR(255) NOT NULL,
+    `name` TEXT NOT NULL , 
+    `company_name` TEXT NOT NULL , 
+    `status` TEXT NOT NULL , 
+    `email` VARCHAR(255) NOT NULL , 
+    `phone_num` INT(10) NOT NULL , 
+    `physical_address` VARCHAR(255) NOT NULL , 
+    `date_in` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE `hospital_db`.`expenses_category` (
+    `id` INT(255) NOT NULL AUTO_INCREMENT , 
+    `name` TEXT NOT NULL , 
+    `status` TEXT NOT NULL , 
+    `description` VARCHAR(255) NOT NULL ,  
+    `date_added` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE `hospital_db`.`expenses` (
+    `id` INT(255) NOT NULL AUTO_INCREMENT , 
+    `name` TEXT NOT NULL , 
+    `payment_method` TEXT NOT NULL , 
+    `paid_from_account` VARCHAR(255) NOT NULL ,
+    `description` VARCHAR(255) NOT NULL ,  
+    `amount` INT NOT NULL ,
+    `status` TEXT NOT NULL ,
+    `due_date` DATE NOT NULL ,
+    `date_added` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE `hospital_db`.`drugs` (
+    `id` INT(255) NOT NULL AUTO_INCREMENT , 
+    `name` TEXT NOT NULL , 
+    `category` TEXT NOT NULL , 
+    `unit_price` INT(255) NOT NULL ,
+    `supplier` VARCHAR(255) NOT NULL ,  
+    `status` TEXT NOT NULL ,
+    `date_added` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE `hospital_db`.`notices` (
+    `id` INT(255) NOT NULL AUTO_INCREMENT , 
+    `title` TEXT NOT NULL ,
+    `description` VARCHAR(255) NOT NULL ,  
+    `start_date` DATE NOT NULL ,
+    `end_date` DATE NOT NULL ,
+    `status` TEXT NOT NULL ,
+    `date_added` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE `hospital_db`.`messages` (
+    `id` INT(255) NOT NULL AUTO_INCREMENT , 
+    `subject` VARCHAR NOT NULL ,
+    `to_email` VARCHAR(255) NOT NULL ,  
+    `from_email` VARCHAR(255) NOT NULL ,
+    `body` VARCHAR(255) NOT NULL ,
+    `attachments` VARCHAR(255) NOT NULL,
+    `date_added` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
