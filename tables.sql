ed39a104e72d5d0ed8698aea9b225c3816f178f0
ealpha072 <ealpha072@gmail.com>
2022-08-08 22:35:06 +0300

    Created employee table

--- tables.sql
+++ tables.sql
@@ -13,3 +13,23 @@ CREATE TABLE `hospital_db`.`patients` (
     `date_in` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP , 
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
+
+CREATE TABLE `hospital_db`.`employees` (
+    `id` INT(255) NOT NULL AUTO_INCREMENT , 
+    `first_name` TEXT NOT NULL , 
+    `last_name` TEXT NOT NULL , 
+    `age` INT(3) NOT NULL , 
+    `sex` TEXT NOT NULL , 
+    `marital_status` TEXT NOT NULL , 
+    `email` VARCHAR(255) NOT NULL , 
+    `phone_num` INT(10) NOT NULL , 
+    `physical_address` VARCHAR(255) NOT NULL , 
+    `dob` DATE NOT NULL , 
+    `nhif_num` INT(15) NOT NULL , 
+    `picture` VARCHAR(255) NOT NULL, 
+    `role` VARCHAR(255) NOT NULL,
+    `kra_num` VARCHAR(255) NOT NULL,
+    `nssf_num` VARCHAR(255) NOT NULL,
+    `date_in` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
